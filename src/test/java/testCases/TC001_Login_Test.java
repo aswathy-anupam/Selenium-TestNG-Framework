c7cb7bd4636cce2439ac1439a5d812aab5454acc
aswathy-anupam <aswathy.anupam@gmail.com>
2025-04-30 14:46:38 +0200

    Update TC001_Login_Test.java

--- src/test/java/testCases/TC001_Login_Test.java
+++ src/test/java/testCases/TC001_Login_Test.java
@@ -17,7 +17,7 @@ public class TC001_Login_Test extends Baseclass
 	{
          try
          {
-		 Loginpage lp=new Loginpage(driver);
+		 Loginpage lp=new Loginpage(driverr);
 		 System.out.println("Checking valid Login");
 		 lp.setUsername(uname);
 		 lp.setPassword(pwd);
