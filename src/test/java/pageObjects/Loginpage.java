6717572e111f8697ac052bf7c53eff61313f2c64
unknown <your-email@example.comaswathy.anup@gmailcom>
2025-05-03 21:59:51 +0200

    Fix Jenkinsfile

--- src/test/java/pageObjects/Loginpage.java
+++ src/test/java/pageObjects/Loginpage.java
@@ -21,7 +21,7 @@ public class Loginpage extends Basepage
 		WebElement btnLogin;
 		
 		@FindBy(xpath="//input[@id='login-button']")
-		WebElement btnLogin5;
+		WebElement btnLogin6;
 				
 		public void setUsername(String uname)
 		{   System.out.println("Inside page object class> first method");
