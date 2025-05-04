302576156c02ff0b191a3dcb577512328f4e71ed
unknown <your-email@example.comaswathy.anup@gmailcom>
2025-05-04 12:30:47 +0200

    For automatic triggering

--- src/test/java/pageObjects/Homepage.java
+++ src/test/java/pageObjects/Homepage.java
@@ -30,7 +30,7 @@ public class Homepage extends Basepage
 	  
 
 	  @FindBy(xpath="//a[@id='logout_sidebar_link']")
-	    WebElement Mnulogout6;
+	    WebElement Mnulogout7;
 	  
 	  public String isHeadingExists()
 	 {
